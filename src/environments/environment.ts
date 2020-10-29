5582d596554fe17b2c7e5898b7edab3155614222
Bryan Ospina <bryospina@gmail.com>
2020-10-29 02:00:31 -0500

    Proyecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDPhru8JsY6hC0iqvDoZXLUnLX_xhdhJNM",
+    authDomain: "angularprueba-4059d.firebaseapp.com",
+    databaseURL: "https://angularprueba-4059d.firebaseio.com",
+    projectId: "angularprueba-4059d",
+    storageBucket: "angularprueba-4059d.appspot.com",
+    messagingSenderId: "805754634747",
+    appId: "1:805754634747:web:32ba202b041f70c80a4b5e",
+    measurementId: "G-6RRF85FSTK"
+  }
+};
